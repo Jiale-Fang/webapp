401746566f9859ada82160138ae9b024ca6acda3
tcefrep <1626680964@qq.com>
2024-02-05 18:54:25 -0500

    Add comments

--- src/main/java/pers/fjl/healthcheck/service/HealthCheckService.java
+++ src/main/java/pers/fjl/healthcheck/service/HealthCheckService.java
@@ -1,5 +1,10 @@
 package pers.fjl.healthcheck.service;
 
 public interface HealthCheckService {
+
+    /**
+     * Check database connection status
+     * @return flag
+     */
     boolean isDatabaseConnected();
 }

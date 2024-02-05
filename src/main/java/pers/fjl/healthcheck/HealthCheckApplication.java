f193d72cfb5e8ed6a1b04d5e012fe41765999f6e
tcefrep <1626680964@qq.com>
2024-02-05 18:55:38 -0500

    Injecting a bean of BCryptPasswordEncoder

--- src/main/java/pers/fjl/healthcheck/HealthCheckApplication.java
+++ src/main/java/pers/fjl/healthcheck/HealthCheckApplication.java
@@ -1,13 +1,22 @@
 package pers.fjl.healthcheck;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@MapperScan("pers.fjl.healthcheck.dao")
 @SpringBootApplication
 public class HealthCheckApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HealthCheckApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }

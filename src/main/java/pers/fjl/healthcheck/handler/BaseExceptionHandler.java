3877139911dc6198267a87f2e5ff6269bc5ec9ba
tcefrep <1626680964@qq.com>
2024-02-06 14:16:00 -0500

    Remove redundant code

--- src/main/java/pers/fjl/healthcheck/handler/BaseExceptionHandler.java
+++ src/main/java/pers/fjl/healthcheck/handler/BaseExceptionHandler.java
@@ -1,23 +1,16 @@
 package pers.fjl.healthcheck.handler;
 
-import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 import org.apache.ibatis.exceptions.PersistenceException;
 import org.mybatis.spring.MyBatisSystemException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import pers.fjl.healthcheck.entity.Result;
-
-import java.net.ConnectException;
 
 @ControllerAdvice
 public class BaseExceptionHandler {

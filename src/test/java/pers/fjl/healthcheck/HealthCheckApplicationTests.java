b5ecadf056da197b81becf572b2bee335f5d5125
tcefrep <1626680964@qq.com>
2024-02-11 12:03:17 -0500

    Add integration tests

--- src/test/java/pers/fjl/healthcheck/HealthCheckApplicationTests.java
+++ src/test/java/pers/fjl/healthcheck/HealthCheckApplicationTests.java
@@ -1,17 +1,69 @@
 package pers.fjl.healthcheck;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.iv.RandomIvGenerator;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class HealthCheckApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testCreateAccount() {
+        String requestBody = "{\"first_name\": \"Jiale\", \"last_name\": \"Fang\", \"username\": \"fjl@example.com\", \"password\": \"123456\"}";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/v1/user")
+                .then()
+                .statusCode(201);
+
+        given()
+                .contentType(ContentType.JSON)
+                .auth()
+                .preemptive()
+                .basic("fjl@example.com", "123456")
+                .when()
+                .get("/v1/user/self")
+                .then()
+                .statusCode(200)
+                .body("first_name", equalTo("Jiale"))
+                .body("last_name", equalTo("Fang"))
+                .body("username", equalTo("fjl@example.com"));
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        // Assuming you have an endpoint for updating user information
+        String requestBody = "{\"first_name\": \"Jayden\", \"last_name\": \"F\", \"password\": \"12345678\"}";
+
+        given()
+                .contentType(ContentType.JSON)
+                .auth()
+                .preemptive()
+                .basic("fjl@example.com", "123456")
+                .body(requestBody)
+                .when()
+                .put("/v1/user/self")
+                .then()
+                .statusCode(204);
+
+        given()
+                .contentType(ContentType.JSON)
+                .auth()
+                .preemptive()
+                .basic("fjl@example.com", "12345678")
+                .when()
+                .get("/v1/user/self")
+                .then()
+                .statusCode(200)
+                .body("first_name", equalTo("Jayden"))
+                .body("last_name", equalTo("F"))
+                .body("username", equalTo("fjl@example.com"));
     }
 
 }

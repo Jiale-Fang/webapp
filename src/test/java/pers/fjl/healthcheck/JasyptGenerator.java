7bd8c8617dba05dd7ebe9c656365538a176b6ff0
tcefrep <1626680964@qq.com>
2024-03-26 11:00:09 -0400

    Fix up

--- src/test/java/pers/fjl/healthcheck/JasyptGenerator.java
+++ src/test/java/pers/fjl/healthcheck/JasyptGenerator.java
@@ -1,8 +1,6 @@
 package pers.fjl.healthcheck;
 
 import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.iv.RandomIvGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
